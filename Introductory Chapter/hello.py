5b991c52a72ba89935a991b40d5525ac8285e098
Wasdns <952693358@qq.com>
2017-02-16 00:55:54 +0800

    add first flask program
    add first flask program

---
+++ Introductory Chapter/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() : 
+	return '<h1>HelloWorld!<h1>'
+
+if __name__ == '__main__':
+	app.run(debug = True)
